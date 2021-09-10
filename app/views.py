e3dded0738a0ef929de08c73557333e1c5a2a8cd
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-10 18:10:00 +0300

    Add template article

--- app/views.py
+++ app/views.py
@@ -6,6 +6,15 @@ from app import app
 def index():
 
     '''
-    View root page function that returns the index page and its data
+    Root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello Moses!!'
+    return render_template('index.html',  message= message)
+
+@app.route('/articles/<article_id>')
+def movie(article_id):
+
+    '''
+    View article page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = article_id)
