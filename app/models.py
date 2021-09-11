7b723db3853ec7d7fdbc6d0cc1b941217858cb89
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-11 17:58:44 +0300

    Add class source

--- app/models.py
+++ app/models.py
@@ -1,4 +1,21 @@
 class Article:
     '''
-    
+    Article class to define Article object
     '''
+    def __init__(self,source, url_img, description, title, author):
+        self.source = source
+        self.url_img = url_img
+        self.description = description
+        self.title = title
+        self.author = author
+
+
+class Source:
+    '''
+    Source class to define Source object
+    '''
+    def __init__(self,id, name, description, url_img):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url_img = url_img
---
+++ tests/test_source.py
@@ -0,0 +1,7 @@
+import unittest
+from app.models import source 
+
+class ArticleTest(unittest. TestCase):
+    '''
+    Test class to test the behaviour of the source class
+    '''
