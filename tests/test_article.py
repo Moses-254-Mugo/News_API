6e11290ff34f63fff47ecbc5f359f6322df329ac
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-11 18:05:50 +0300

    Add a method

--- tests/test_article.py
+++ tests/test_article.py
@@ -5,3 +5,14 @@ class ArticleTest(unittest. TestCase):
     '''
     Test class to test the behaviour of the Article class
     '''
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_article = Article()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_source.py
+++ tests/test_source.py
@@ -1,7 +1,16 @@
 import unittest
-from app.models import source 
+from app.models import Source 
 
 class ArticleTest(unittest. TestCase):
     '''
     Test class to test the behaviour of the source class
     '''
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source()
+
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
